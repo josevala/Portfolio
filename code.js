55f439a2e13da54eeee6f22ac710f30ef90bfc42
jose Valadez <josevala04@gmail,com>
2022-08-21 17:31:29 -0700

    made sections

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
     anchor.addEventListener("click",function(e){
-        e.preventDefault();
+       e.preventDefault();
         document.querySelector(this.getAttribute("href")).scrollIntoView({
             behavior: "smooth"
         });
